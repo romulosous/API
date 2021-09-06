344ccf1d79bb7fdac61c1f6308d1a64b90b59abd
Romulo Carvalho <romulocarvalho081@gmail.com>
2021-09-05 21:26:02 -0300

    setting mandatory parameters

--- src/routes.ts
+++ src/routes.ts
@@ -8,5 +8,10 @@ export function createCourse(request: Request, response: Response) {
     duration: 10,
   });
 
+  CreateCourseService.execute({
+    name: "ReactJS",
+    educator: "Diego",
+  });
+
   return response.send();
 }
